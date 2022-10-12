a36bc52ff9762b9335a9e00dcb75dd5a40a05c05
21377 <t@vtsd.com>
2022-10-12 07:47:38 -0400

    lab 4b

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -13,10 +13,20 @@ public class Lab04bvst extends Applet
     public void paint(Graphics g)
     {
         // DRAW CUBE
+g.drawLine(150,125,150,225);
+g.drawLine(150,225,200,275);
+g.drawLine(200,275,200,175);
+g.drawLine(200,175,150,125);
+g.drawLine(150,125,250,125);
+g.drawLine(250,125,250,225);
+g.drawLine(250,225,150,225);
+g.drawLine(250,125,300,175);
+g.drawLine(300,175,200,175);
+g.drawLine(300,175,300,275);
+g.drawLine(300,275,200,275);
+g.drawLine(250,225,300,275);
 
-
-
-        // DRAW SPHERE
+// DRAW SPHERE
 
 
 
