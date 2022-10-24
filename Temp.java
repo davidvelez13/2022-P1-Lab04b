ab4348623a812ad62a6f1adf406f1af0ee150f88
21377 <t@vtsd.com>
2022-10-24 07:42:32 -0400

    lab 4b

---
+++ Temp.java
@@ -0,0 +1,11 @@
+public class Temp {
+    public static void main(String args[]) {
+
+        for (int k = 0; k<= 100; k += 3) {
+            int sum = 100 - k;
+            System.out.print(sum + ", ");
+
+
+        }
+    }
+}
